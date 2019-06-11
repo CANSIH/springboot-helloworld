bfafebe19c36ecca2a2decb2fb769eeb18e0787a
CongGang <CongGang@DESKTOP-MNKI5EO>
2019-06-11 22:10:22 +0800

    修改日志

--- src/main/java/com/hecg/springboothelloworld/SpringbootHelloworldApplication.java
+++ src/main/java/com/hecg/springboothelloworld/SpringbootHelloworldApplication.java
@@ -8,8 +8,8 @@ public class SpringbootHelloworldApplication {
 
 	
 	public static void main(String[] args) {
-		System.out.println("开始启动spring boot........");
+		System.out.println("=========================spring boot 开始启动=========================");
 		SpringApplication.run(SpringbootHelloworldApplication.class, args);
-		System.out.println("spring boot启动完成........");
+		System.out.println("=========================spring boot 启动完成=========================");
 	}
 }
